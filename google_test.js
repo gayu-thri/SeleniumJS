bf366171d9fae3fe3cf1f75b794b3dc00a86f4b0
Gayathri Ethiraj <39553592+gayu-thri@users.noreply.github.com>
2020-01-16 16:46:39 +0530

    Sample webdriver google search

--- google_test.js
+++ google_test.js
@@ -6,22 +6,29 @@ const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
+    //and now using the instance "driver" of selenium webdriver
+    //visit google
 driver.get('http://www.google.com');
 
+//find the text input element by its name
 driver.findElement(By.name('q')).sendKeys('webdriver');
-
-driver.sleep(1000).then(function() {
-  driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
+driver.sleep(1000).then(function() 
+{
+  driver.findElement(By.name('q')).sendKeys(webdriver.Key.RETURN);
 });
+//You should see an instance of Chrome automatically open up! Google should automatically be loaded in a tab, "webdriver" should be entered in the search box, and the search button will be clicked. 
 driver.findElement(By.name('btnK')).click();
 
-driver.sleep(2000).then(function() {
+console.log("Page title is: " + driver.getTitle())
+
+driver.sleep(2000).then(function() 
+{
   driver.getTitle().then(function(title) {
     if(title === 'webdriver - Google Search') {
-      console.log('Test passed');
+      console.log('Test passed'); // if it is "webdriver - Google Search", we will return a message to claim the test is passed.
     } else {
       console.log('Test failed');
     }
-    driver.quit();
+    driver.quit(); //WebDriver will then close down the chrome instance and stop.
   });
 });
