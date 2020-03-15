d6d2722f598060deccb628ce64feebd9fc399ddc
Gayathri Ethiraj <39553592+gayu-thri@users.noreply.github.com>
2020-03-15 22:48:06 +0530

    Updated login positive test case for the new UI

--- login_tests/login_test.js
+++ login_tests/login_test.js
@@ -6,35 +6,26 @@ let driver =await new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
-await driver.get('https://iris-se.netlify.com/login')
+//await driver.get('https://iris-se.netlify.com/login')
+await driver.get('https://iris-beta.netlify.com/')  // changed UI
+await driver.findElement(By.name('email')).sendKeys('saibalsu@gmail.com')
+    await driver.findElement(By.name('password')).sendKeys('password123')
 
-await driver.findElement(By.xpath("//*[@type=\"text\"]")).sendKeys('egayu3@gmail.com');
-await driver.findElement(By.xpath("//*[@type=\"password\"]")).sendKeys('pass');
-/*
-driver.sleep(1000).then(function() 
+    await(await driver.findElement(By.xpath('/html/body/div/div/div/div/div/div/div/form/div[4]/button'))).click()
+
+    await driver.getPageSource().then(function(content) 
 {
-    driver.findElement(By.name('q')).sendKeys(webdriver.Key.RETURN);
-});
-*/
-
-await await(driver.findElement(By.id("loginButton"),20000).click());
-
-
-await d.getPageSource().then(function(content) 
-{
-  if(content.indexOf('Welcome ') !==-1 ) {
-   console.log('Test passed');
+  if(content.indexOf('Saiharsha B ') !==-1 ) {
+   console.log('Test failed');
    /*console.log('Target page');
    d.get('https://iris-se.netlify.com') */
   } else {
-      console.log('Test failed');
+      console.log('Test passed');
       return false;
   }
-  //driver.quit();
+  driver.quit();
+
+  
 });
 }
 login_test1();
-
-/*driver.wait(webdriver.until.elementLocated(By.id("logged-in-message")), 10000).getText().then(function(txt){
-    console.log("test success: "+txt);
-})  */
